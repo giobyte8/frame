e42e99089e428652be403c4660dbd406f67794cc
Giovanni Aguirre <giovanni.fi05@gmail.com>
2026-03-15 20:06:36 -0600

    Types for API entities

--- src/types/api.ts
+++ src/types/api.ts
@@ -4,6 +4,30 @@ export interface Folder {
   imageCount: number;
 }
 
+export type UUID = `${string}-${string}-${string}-${string}-${string}`;
+
+export interface Directory {
+  id: UUID;
+  path: string;
+  recursive: boolean;
+  status: string;
+  version: number;
+}
+
+export type ISODateString = string;
+
+export interface ImageModel {
+  cameraMaker?: string | null;
+  cameraModel?: string | null;
+  contentHash: string;
+  datetimeOriginal?: ISODateString | null;
+  gpsLatitude?: number | null;
+  gpsLongitude?: number | null;
+  path: string;
+  status: string;
+  version: number;
+}
+
 export interface ImageMetadata {
   width: number;
   height: number;
@@ -13,4 +37,12 @@ export interface Image {
   id: string;
   url: string;
   metadata: ImageMetadata;
+}
+
+export interface Page<T> {
+  content: T[];
+  pageIdx: number;
+  pageSize: number;
+  totalPages: number;
+  totalElements: number;
 }
